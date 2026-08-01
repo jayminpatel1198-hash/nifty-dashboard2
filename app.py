d112dbb57b8d5630b9cfe4343d90ca647fbb70a1
jayminpatel1198-hash <jayminpatel1198@gmail.com>
2026-08-01 23:50:44 +0530

    Block 2
    Added functions to fetch JSON data and retrieve spot prices.

--- app.py
+++ app.py
@@ -18,3 +18,43 @@ OPTION_CHAIN_URL = "https://api.upstox.com/v2/option/chain"
 CONTRACT_URL = "https://api.upstox.com/v2/option/contract"
 
 STEP = 50
+def get_json(url, params=None):
+    r = requests.get(
+        url,
+        headers=HEADERS,
+        params=params,
+        timeout=10
+    )
+
+    r.raise_for_status()
+    return r.json()
+
+
+def num(x):
+    try:
+        return float(x)
+    except:
+        return 0.0
+
+
+def get_spot():
+    data = get_json(
+        QUOTE_URL,
+        {
+            "instrument_key": INDEX_KEY
+        }
+    )
+
+    quotes = data.get("data", {})
+    info = quotes.get(INDEX_KEY, {})
+
+    ltp = num(info.get("last_price"))
+
+    if ltp == 0:
+        ltp = num(info.get("ltp"))
+
+    if ltp == 0:
+        ohlc = info.get("ohlc", {})
+        ltp = num(ohlc.get("close"))
+
+    return ltp
